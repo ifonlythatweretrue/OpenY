9976449eba8abf4ff838f89ed2be65afcf97814c
jasonnguyen <neilgroup101@gmail.com>
2025-04-03 14:34:11 -0400

    start

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main {
+    public static boolean login(){
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if(login()){
+
+        }
+        //make login function
+    }
+}
